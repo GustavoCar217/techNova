9bb6b587a33851140e55a406dc4520034962c4dc
PROGRAMAÇÃO EM PYTHON - 2024.4 <47129532024.4@senacrjedu.local>
2024-12-13 09:49:51 -0300

    atualização

--- techNova/settings.py
+++ techNova/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-465euu-ix&^yh0*ww+_x_4mvg_tb(8)(u%rjv-x+*mjh#$1)b-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['technova-gj3z.onrender.com']
+ALLOWED_HOSTS = ['technova-gj3z.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
